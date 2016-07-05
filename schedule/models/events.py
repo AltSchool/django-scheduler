19ad642731d8979d1bb63072ddc47ebda4bd1693
Alex Madjar <alexm@altschool.com>
2016-07-05 14:46:27 -0700

    Make Django 1.9 compatible

--- schedule/models/events.py
+++ schedule/models/events.py
@@ -3,7 +3,7 @@ from django.conf import settings as django_settings
 import pytz
 from dateutil import rrule
 
-from django.contrib.contenttypes import generic
+from django.contrib.contenttypes.fields import GenericForeignKey
 from django.db import models
 from django.db.models import Q
 from django.contrib.contenttypes.models import ContentType
@@ -420,7 +420,7 @@ class EventRelation(models.Model):
     event = models.ForeignKey(Event, verbose_name=_("event"))
     content_type = models.ForeignKey(ContentType)
     object_id = models.IntegerField()
-    content_object = generic.GenericForeignKey('content_type', 'object_id')
+    content_object = GenericForeignKey('content_type', 'object_id')
     distinction = models.CharField(_("distinction"), max_length=20, null=True)
 
     objects = EventRelationManager()
