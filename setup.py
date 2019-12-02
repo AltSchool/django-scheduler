86ad145f192cab4c98c58e39ceb37aa5c8ffe84e
Savinay Nangalia <savinay.ng@gmail.com>
2019-12-02 14:43:55 -0800

    update min and max django version

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
                  'Topic :: Utilities'],
     install_requires=[
         'python-dateutil>=2.1.post20140303',
-        'Django>=1.7,<=1.11',
+        'Django>=1.8,<=2.0',
         'argparse>=1.1',
         'pytz>=2013.9',
         'six>=1.3.0',
